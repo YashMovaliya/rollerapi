8eb88e9d0d495a4ebaa2d9f1b2beca4370acbdbb
yashmovaliya1607@gmail.com <yashmovaliya1607@gmail.com>
2020-11-06 19:10:47 +0530

    some changes

--- server.go
+++ server.go
@@ -1,32 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type Coaster struct {
-	Name			string `json:"name"`
-	Manufacturer	string `json:"manufacturer"`
-	ID				string `json:"id"`
-	InPark			string `json:"inPark"`
-	Height			string `json:"height"`
+	Name         string `json:"name"`
+	Manufacturer string `json:"manufacturer"`
+	ID           string `json:"id"`
+	InPark       string `json:"inPark"`
+	Height       int    `json:"height"`
 }
 
 type coasterHandlers struct {
-	store map[string] Coaster 
+	store map[string]Coaster
 }
 
-func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request){
+func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
+	coasters := make([]Coaster, len(h.store))
+	i := 0
+	for _, coaster := range h.store {
+		coasters[i] = coaster
+		i++
+	}
 
+	jsonBytes, err := json.Marshal(coasters)
+	if err != nil {
+
+	}
+	w.Write(jsonBytes)
 }
 
 func newCoasterHandlers() *coasterHandlers {
 	return &coasterHandlers{
-		store : map[string] Coaster{
+		store: map[string]Coaster{
 			"id1": Coaster{
-				Name: "Fury 325",
-				Height: 99,
-				ID: "id1",
-				InPark: "Carowimds",
-				Manufacturer: "B+M"
+				Name:         "Fury 325",
+				Height:       99,
+				ID:           "id1",
+				InPark:       "Carowimds",
+				Manufacturer: "B+M",
 			},
 		},
 	}
@@ -40,4 +54,3 @@ func main() {
 		panic(err)
 	}
 }
-
